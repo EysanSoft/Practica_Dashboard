4bd724b1210e42db521d6b4be722eaa876ee1a9e
EysanSoft <ivanhersan16@gmail.com>
2025-02-07 12:53:00 -0600

    La gráfica de mensajes enviados ahora deshabilita los meses sin registros.

--- js/custom_scripts/reportes.js
+++ js/custom_scripts/reportes.js
@@ -58,8 +58,6 @@ $(document).ready(function () {
 /*
   Esta función genera un gráfico de barras de todos los mensajes enviados al mes.
   Recibe un arreglo bidimensional de conteos de mensajes por tipo segun el mes de registro.
-  NOTA: Falta implementar si existen datos en el resto de los meses, y mostrar los meses
-  según el año actual.
 */
 function graficaMensajesMensuales(conteoMensual) {
   const mensajesMensuales = Highcharts.chart("graficaMensajesMensuales", {
@@ -75,7 +73,13 @@ function graficaMensajesMensuales(conteoMensual) {
     yAxis: {
       title: {
         text: "Conteo Mensual",
+        align: 'high'
       },
+    },
+    plotOptions: {
+      bar: {
+          borderRadius: '30%',
+      }
     },
     series: [
       {
@@ -86,6 +90,55 @@ function graficaMensajesMensuales(conteoMensual) {
         name: "Febrero",
         data: conteoMensual[1],
       },
+      {
+        name: "Marzo",
+        data: conteoMensual[2],
+      },
+      {
+        name: "Abril",
+        data: conteoMensual[3],
+      },
+      {
+        name: "Mayo",
+        data: conteoMensual[4],
+      },
+      {
+        name: "Junio",
+        data: conteoMensual[5],
+      },
+      {
+        name: "Julio",
+        data: conteoMensual[6],
+      },
+      {
+        name: "Agosto",
+        data: conteoMensual[7],
+      },
+      {
+        name: "Septiembre",
+        data: conteoMensual[8],
+      },
+      {
+        name: "Octubre",
+        data: conteoMensual[9],
+      },
+      {
+        name: "Noviembre",
+        data: conteoMensual[10],
+      },
+      {
+        name: "Diciembre",
+        data: conteoMensual[11],
+      },
     ],
   });
+  let j = 0;
+  // Ocultar series sin datos en el resto de los meses.
+  $(mensajesMensuales.series).each(function() {
+    if(typeof conteoMensual[j] === 'undefined') {
+      this.setVisible(false, false);
+    }
+    j += 1;
+  });
+  mensajesMensuales.redraw();
 }
